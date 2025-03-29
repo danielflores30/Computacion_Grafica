1d3d017657efad480b2b37a61b5dbf9e0ce4e43b
danielflores30 <danielef3006@gmail.com>
2025-03-28 23:10:54 -0600

    Update Iluminación.cpp

--- Iluminacion.cpp
+++ Iluminacion.cpp
@@ -1,8 +1,7 @@
-//Materiales e Iluminaci�n                   Escobar Flores Daniel
-//Fecha de Entrega: 22/03/2025                     318187952
- 
- 
- 
+﻿//Práctica 08n Materiales e Iluminación                   Escobar Flores Daniel
+//Fecha de Entrega: 28/03/2025                                  318187952
+
+
 // Std. Includes
 #include <string>
 
@@ -26,7 +25,7 @@
 #include "SOIL2/SOIL2.h"
 #include "stb_image.h"
 // Properties
-const GLuint WIDTH = 800, HEIGHT = 600;
+const GLuint WIDTH = 1366, HEIGHT = 768;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
 // Function prototypes
@@ -36,19 +35,30 @@ void DoMovement();
 
 
 // Camera
-Camera camera(glm::vec3(0.0f, 0.0f, 0.0f));
+Camera camera(glm::vec3(0.0f, 9.0f, 30.0f));
 bool keys[1024];
 GLfloat lastX = 400, lastY = 300;
 bool firstMouse = true;
 
 
 // Light attributes
-glm::vec3 lightPos(0.5f, 0.5f, 2.5f);
-float movelightPos = 0.0f;
+glm::vec3 lightPos1(15.0f, 8.0f, 0.0f);
+glm::vec3 lightPos2(15.0f, 8.0f, 0.0f); // Posici�n del segundo cubo
+//float movelightPos = 0.0f;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 float rot = 0.0f;
 bool activanim = false;
+// Light movement
+float lightAngle = 0.0f;  // �ngulo inicial
+float lightRadius = 15.0f;      // Radio del c�rculo (ajusta seg�n necesites)
+const glm::vec3 dogCenter(0.0f, 9.23f, 0.0f); // Centro del perro (posici�n fija seg�n su modelo)
+bool isMovingForward = false;           // Direcci�n del movimiento
+glm::vec3 activeLightPos;               // Luz activa (pasa al shader)
+// Light colors
+glm::vec3 light1Color = glm::vec3(1.0f, 0.6f, 0.0f);  // Anaranjado
+glm::vec3 light2Color = glm::vec3(0.2f, 0.2f, 1.0f);  // Azul
+glm::vec3 activeLightColor; // Color activo
 
 int main()
 {
@@ -62,7 +72,7 @@ int main()
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Materiales e Iluminacion Escobar Flores Daniel", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Materiales e Iluminacion - Daniel Escobar Flores", nullptr, nullptr);
 
     if (nullptr == window)
     {
@@ -106,7 +116,10 @@ int main()
 
 
     // Load models
-    Model Sun((char*)"Models/Sun.obj");
+    Model dog((char*)"Models/RedDog.obj");
+    Model moon((char*)"Models/Moon.obj");
+    Model forest((char*)"Models/forest.obj");
+    Model sun((char*)"Models/sun.obj");
     glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
     float vertices[] = {
@@ -169,7 +182,8 @@ int main()
 
     // Load textures
 
-    GLuint texture;
+    GLuint texture, textureSun, textureEarth;
+    //Texturas
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
     int textureWidth, textureHeight, nrChannels;
@@ -181,6 +195,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
 
     image = stbi_load("Models/Texture_albedo.jpg", &textureWidth, &textureHeight, &nrChannels, 0);
+
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     if (image)
@@ -194,6 +209,33 @@ int main()
     }
     stbi_image_free(image);
 
+    // Textura del Sol
+    glGenTextures(1, &textureSun);
+    glBindTexture(GL_TEXTURE_2D, textureSun);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    unsigned char* sunImage = stbi_load("Models/sun.png", &textureWidth, &textureHeight, &nrChannels, 0);
+    if (sunImage) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, sunImage);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load sun texture" << std::endl;
+    }
+    stbi_image_free(sunImage);
+
+    unsigned char* moonImage = stbi_load("Models/moon.jpg", &textureWidth, &textureHeight, &nrChannels, 0);
+    if (moonImage) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, moonImage);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load earth texture" << std::endl;
+    }
+    stbi_image_free(moonImage);
 
     // Game loop
     while (!glfwWindowShouldClose(window))
@@ -211,56 +253,89 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        
+        float angleRadians = glm::radians(lightAngle);
+
+        // Primer cubo 
+        float cube1Angle = glm::min(lightAngle, 180.0f);
+        lightPos1 = dogCenter + lightRadius * glm::vec3(cos(glm::radians(cube1Angle)), sin(glm::radians(cube1Angle)), 0.0f);
+
+        // Segundo cubo 
+        float cube2Angle = glm::max(lightAngle - 180.0f, 0.0f);
+        lightPos2 = dogCenter + lightRadius * glm::vec3(cos(glm::radians(cube2Angle)), sin(glm::radians(cube2Angle)), 0.0f);
+
+        // Determinar luz activa y su color
+        if (isMovingForward) {
+            activeLightPos = (lightAngle <= 180.0f) ? lightPos1 : lightPos2;
+            activeLightColor = (lightAngle <= 180.0f) ? light1Color : light2Color;
+        }
+        else {
+            activeLightPos = (lightAngle >= 180.0f) ? lightPos2 : lightPos1;
+            activeLightColor = (lightAngle >= 180.0f) ? light2Color : light1Color;
+        }
+
         lightingShader.Use();
-        GLint lightPosLoc = glGetUniformLocation(lightingShader.Program, "light.position");
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.position"), activeLightPos.x, activeLightPos.y, activeLightPos.z);
         GLint viewPosLoc = glGetUniformLocation(lightingShader.Program, "viewPos");
-        glUniform3f(lightPosLoc, lightPos.x + movelightPos, lightPos.y + movelightPos, lightPos.z + movelightPos);
+        //glUniform3f(lightPosLoc, lightPos.x + movelightPos, lightPos.y + movelightPos, lightPos.z + movelightPos);*/
         glUniform3f(viewPosLoc, camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z);
 
 
         // Set lights properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.3f, 0.3f, 0.3f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 0.3f, 0.3f, 0.3f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 0.0f, 0.0f, 0.0f);
-
+        
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), activeLightColor.x * 0.1f, activeLightColor.y * 0.1f, activeLightColor.z * 0.1f);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), activeLightColor.x * 0.6f, activeLightColor.y * 0.6f, activeLightColor.z * 0.6f);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), activeLightColor.x * 0.6f, activeLightColor.y * 0.6f, activeLightColor.z * 0.6f);
 
         glm::mat4 view = camera.GetViewMatrix();
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-        // Set material properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.ambient"), 0.5f, 0.5f, 0.5f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.diffuse"), 0.8f, 0.8f, 0.0f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.specular"), 1.0f, 1.0f, 1.0f);
-        glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 0.8f);
 
+        // Set material properties
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.ambient"), 0.1f, 0.1f, 0.1f);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.diffuse"), 1.0f, 1.0f, 1.0f);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.specular"), 5.0f, 5.0f, 5.0f);
+        glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 0.6f);
 
 
         // Draw the loaded model
-        glm::mat4 model(1);
-        model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
-        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glBindVertexArray(VAO);
+        //Luna
+        glm::mat4 forestModel(1);
+        forestModel = glm::rotate(forestModel, glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        forestModel = glm::rotate(forestModel, glm::radians(75.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        forestModel = glm::scale(forestModel, glm::vec3(5.0f, 5.0f, 5.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(forestModel));
+        forest.Draw(lightingShader);
+        //Perro
+        glm::mat4 dogModel(1);
+        dogModel = glm::translate(dogModel, glm::vec3(0.0f, 9.23f, 0.0f));
+        dogModel = glm::scale(dogModel, glm::vec3(1.5f, 1.5f, 1.5f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(dogModel));
+        dog.Draw(lightingShader);
+      
 
-        //glDrawArrays(GL_TRIANGLES, 0, 36);
-        
-        //Carga de RedDog
-        Sun.Draw(lightingShader);
+
         glBindVertexArray(0);
-
-
 
 
         lampshader.Use();
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos + movelightPos);
-        model = glm::scale(model, glm::vec3(0.3f));
-        glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+       
+        glActiveTexture(GL_TEXTURE0); // Activar la unidad de textura 0
+        glBindTexture(GL_TEXTURE_2D, textureSun); // Vincular textura del Sol
+        glm::mat4 sunModel = glm::mat4(1.0f);
+        sunModel = glm::translate(sunModel, lightPos1);
+        sunModel = glm::scale(sunModel, glm::vec3(0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(sunModel));
+        sun.Draw(lampshader);
+        
+        glActiveTexture(GL_TEXTURE0); // Misma unidad de textura (o usa GL_TEXTURE1 si prefieres)
+       
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
         glBindVertexArray(0);
 
         // Swap the buffers
@@ -305,6 +380,19 @@ void DoMovement()
             rot -= 0.1f;
     }
 
+    float speed = 50.0f; // Ajusta la velocidad
+
+    if (keys[GLFW_KEY_O]) {
+        lightAngle += speed * deltaTime; // Avance (0� -> 360�)
+    }
+    if (keys[GLFW_KEY_L]) {
+        lightAngle -= speed * deltaTime; // Retroceso (360� -> 0�)
+    }
+
+    // Limitar el �ngulo entre 0� y 360�
+    lightAngle = glm::clamp(lightAngle, 0.0f, 360.0f);
+
+
 }
 
 // Is called whenever a key is pressed/released via GLFW
@@ -326,20 +414,6 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
             keys[key] = false;
         }
     }
-
-    if (keys[GLFW_KEY_O])
-    {
-       
-        movelightPos += 0.1f;
-    }
-
-    if (keys[GLFW_KEY_L])
-    {
-        
-        movelightPos -= 0.1f;
-    }
-
-
 }
 
 void MouseCallback(GLFWwindow* window, double xPos, double yPos)
@@ -359,5 +433,3 @@ void MouseCallback(GLFWwindow* window, double xPos, double yPos)
 
     camera.ProcessMouseMovement(xOffset, yOffset);
 }
-
-
